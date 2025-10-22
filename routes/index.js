e3697fc97c52ed7230eb48e402aa7ccebf3d394f
Helena <helena200805@gmail.com>
2025-10-22 16:14:39 +0700

    api labs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersRouter = require('./users.router');
 const arrticleRouter = require('./articles.route');
 const commentRouter = require('./comment.router');
+const labsRouter = require('./labs.router');
 
 // Handle article ID parameter
 router.param('id', (req, res, next, id) => {
@@ -13,5 +14,6 @@ router.param('id', (req, res, next, id) => {
 router.use('/api/users', usersRouter);
 router.use('/api/article/:id/comment', commentRouter);
 router.use('/api/article', arrticleRouter);
+router.use('/api/labs', labsRouter);
 
 module.exports = router;
