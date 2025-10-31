60412c91ab3b145429a65c2a408bed4c01d613da
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-10-31 10:32:29 +0700

    feat: add endpoint for game feedback operations

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ const miniGamesRouter = require('./miniGames.router');
 const scoresRouter = require('./scores.router');
 const memoCardRouter = require('./memoCard.router');
 const dragNDropRouter = require('./dragNDrop.router');
+const gameFeedbackRouter = require('./gameFeedback.routes');
 
 
 // Handle article ID parameter
@@ -34,6 +35,7 @@ router.use('/api/missions', missionRouter);
 router.use('/api/scores', scoresRouter);
 router.use('/api/memo-cards', memoCardRouter);
 router.use('/api/drag-n-drop', dragNDropRouter);
+router.use('/api/game-feedback', gameFeedbackRouter);
 
 
 module.exports = router;
